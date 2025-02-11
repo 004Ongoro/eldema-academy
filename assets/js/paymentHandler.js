aeceb2cb29ef6c0b3d88c35ecc67785d79798753
George Ongoro <gtechong72@gmail.com>
2025-02-11 03:36:37 -0800

    Updated.

--- assets/js/paymentHandler.js
+++ assets/js/paymentHandler.js
@@ -2,6 +2,10 @@ const backdrop = document.getElementById("staticBackdrop");
 
 document.getElementById("btnDonate").addEventListener("click", () => {
   const amount = document.getElementById("inp-donation-amount").value;
-  confirm(`Confirm payment of KES ${amount}`);
+  if (confirm(`Confirm payment of KES ${amount}`) === true) {
+    console.log("Accepted");
+  } else {
+    console.log("Denied");
+  }
   backdrop.setAttribute("aria-hidden", "true");
 });
