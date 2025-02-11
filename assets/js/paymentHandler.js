a7b049c4642d8226ae726a74f2c59b6037c5f906
George Ongoro <gtechong72@gmail.com>
2025-02-11 03:50:27 -0800

    Open graph image add.

--- assets/js/paymentHandler.js
+++ assets/js/paymentHandler.js
@@ -4,8 +4,10 @@ document.getElementById("btnDonate").addEventListener("click", () => {
   const amount = document.getElementById("inp-donation-amount").value;
   if (confirm(`Confirm payment of KES ${amount}`) === true) {
     console.log("Accepted");
+    // Go endpoint to initialize mpesa stk
   } else {
     console.log("Denied");
+    //return
   }
   backdrop.setAttribute("aria-hidden", "true");
 });
